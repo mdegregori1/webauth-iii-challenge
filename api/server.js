0eba34213629b61a81593cbc3b349b13999a29a2
Mauricio Degregori <degregori2@gmail.com>
2019-12-18 18:41:47 -0600

    updated all files before writing routers/models

--- api/server.js
+++ api/server.js
@@ -1,12 +1,10 @@
 const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
-// require('dotenv').config() // means we can create a dotenv file
 
-// console.log(process.env.JWT_SECRET)
 
-// const authRouter = require('../auth/auth-router.js');
-// const usersRouter = require('../users/users-router.js');
+const authRouter = require('../auth/auth-router.js');
+const usersRouter = require('../users/users-router.js');
 
 const server = express();
 
@@ -14,8 +12,10 @@ server.use(helmet());
 server.use(express.json());
 server.use(cors());
 
-// server.use('/api/auth', authRouter);
-// server.use('/api/users', usersRouter);
+
+server.use('/api/auth', authRouter);
+server.use('/api/users', usersRouter);
+
 
 server.get('/', (req, res) => {
   res.send("test test test :)");
---
+++ auth/auth-router.js
@@ -0,0 +1,62 @@
+const router = require("express").Router();
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken"); // installed this
+
+const Users = require("../users/users-model.js");
+
+// for endpoints beginning with /api/auth
+// router.post("/register", (req, res) => {
+//   let user = req.body;
+//   const hash = bcrypt.hashSync(user.password, 10); // 2 ^ n
+//   user.password = hash;
+
+//   Users.add(user)
+//     .then(saved => {
+//       res.status(201).json(saved);
+//     })
+//     .catch(error => {
+//       res.status(500).json(error);
+//     });
+// });
+
+// router.post("/login", (req, res) => {
+//   let { username, password } = req.body;
+
+//   Users.findBy({ username })
+//     .first()
+//     .then(user => {
+//       if (user && bcrypt.compareSync(password, user.password)) {
+//         // sign token
+//         const token = signToken(user); // new line
+
+//         // send the token
+//         res.status(200).json({
+//           token, // added token as part of the response sent
+//           message: `Welcome ${user.username}!`,
+//         });
+//       } else {
+//         res.status(401).json({ message: "Invalid Credentials" });
+//       }
+//     })
+//     .catch(error => {
+//       res.status(500).json(error);
+//     });
+// });
+
+// // this functions creates and signs the token
+// function signToken(user) {
+//   const payload = {
+//     username: user.username,
+//     role: "student", // this will come from the database users.role
+//   };
+
+//   const secret = process.env.JWT_SECRET || "is it secret, is it safe?";
+
+//   const options = {
+//     expiresIn: "1h",
+//   };
+
+//   return jwt.sign(payload, secret, options); // notice the return
+// }
+
+module.exports = router;
